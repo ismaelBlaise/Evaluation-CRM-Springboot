b64ba1b7a4a9c7dfd0672cb788551e2fcdb18d75
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-22 17:06:22 +0300

    ajout des fonctions de login

--- src/main/java/com/crm/evaluation/controllers/LoginController.java
+++ src/main/java/com/crm/evaluation/controllers/LoginController.java
@@ -1,10 +1,32 @@
 package com.crm.evaluation.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.crm.evaluation.services.LoginService;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-        
+    @Autowired
+    private LoginService loginService;
+    
+
+    @PostMapping
+    public ModelAndView login(@RequestParam String email,@RequestParam String password){
+        ModelAndView modelAndView=new ModelAndView("template");
+
+        try {
+            loginService.login(email, password);
+            modelAndView.addObject("page","dashboard" );
+        } catch (Exception e) {
+            modelAndView.setViewName("login");
+            modelAndView.addObject("erreur", e.getMessage());
+        }
+        return modelAndView;
+    }
 }
