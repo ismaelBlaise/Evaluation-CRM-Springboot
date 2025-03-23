fab52f2491614d56c5bba6351a318d58685bb175
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-23 07:35:04 +0300

    ok

--- src/main/java/com/crm/evaluation/services/DashBoardService.java
+++ src/main/java/com/crm/evaluation/services/DashBoardService.java
@@ -99,4 +99,36 @@ public class DashboardService {
             throw new Exception(e.getMessage());
         }
     }
+
+
+
+    @SuppressWarnings({ "unchecked", "deprecation" })
+    public Map<String, Object> getMonthlyRevenueChart() throws Exception {
+         
+        String url = apiBaseUrl + "/payments/chart"; 
+        
+        RestTemplate restTemplate = new RestTemplate();
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+            
+            if (response.getStatusCodeValue() != 200) {
+                throw new Exception("Erreur lors de la récupération des données.");
+            }
+            
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> revenueData = objectMapper.readValue(response.getBody(), Map.class);
+            
+            return revenueData; 
+        } catch (HttpClientErrorException e) {
+            throw new Exception("Erreur lors de l'appel API pour les revenus mensuels.");
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
 }
