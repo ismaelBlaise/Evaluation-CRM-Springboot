d5b9aa34f2599927d136582cae24ad5a2e2e4208
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-23 14:12:20 +0300

    fin des changement

--- src/main/java/com/crm/evaluation/controllers/InvoiceApiController.java
+++ src/main/java/com/crm/evaluation/controllers/InvoiceApiController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crm.evaluation.services.DashboardService;
@@ -18,10 +17,13 @@ public class InvoiceApiController {
     @Autowired
     private DashboardService dashboardService;
 
-   @GetMapping("/chart")
-    public Map<String, Double> getInvoicePaymentSummary(
-            @RequestParam(name = "annee", required = false) String annee,
-            @RequestParam(name = "mois", required = false) String mois) throws Exception {
-             return dashboardService.getInvoicePaymentSummary(annee, mois);
+    @GetMapping("/chart/{annee}/{mois}")
+    public Map<String, Double> getInvoicePaymentSummary(@PathVariable(required = false) String annee, @PathVariable(required = false)   String mois) throws Exception {
+        return dashboardService.getInvoicePaymentSummary(annee, mois);
+    }
+
+    @GetMapping("/chart")
+    public Map<String, Double> getInvoicePaymentSummary2() throws Exception {
+        return dashboardService.getInvoicePaymentSummary(null, null);
     }
 }
