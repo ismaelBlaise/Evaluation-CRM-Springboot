16fe7edb97e30ba2d23c9f98df9e6cea982c5d48
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-24 21:23:13 +0300

    ok

--- src/main/java/com/crm/evaluation/services/ClientService.java
+++ src/main/java/com/crm/evaluation/services/ClientService.java
@@ -1,33 +1,49 @@
 package com.crm.evaluation.services;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.crm.evaluation.responses.ClientResponse;
-import com.crm.evaluation.responses.PaymentResponse;
 
 @Service
 public class ClientService {
+    
     @Value("${api.base.url}")
     private String apiBaseUrl; 
 
     @Autowired
     private RestTemplate restTemplate;
-   
+    
+    @Autowired
+    private HttpSession session;  
 
-   
-
-    public ClientResponse getClients(int page,int perPage) {
-        @SuppressWarnings("deprecation")
-        String url = UriComponentsBuilder.fromHttpUrl(apiBaseUrl+"/clients")
+    public ClientResponse getClients(int page, int perPage) {
+        String url = UriComponentsBuilder.fromHttpUrl(apiBaseUrl + "/clients")
                 .queryParam("per_page", perPage)
-                .queryParam("page",page)
+                .queryParam("page", page)
                 .toUriString();
 
-        return restTemplate.getForObject(url, ClientResponse.class);
-    }
+        
+        String token = (String) session.getAttribute("token");
+        if (token == null) {
+            throw new RuntimeException("Token non trouvé en session !");
+        }
 
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+        
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        
+        // Effectuer la requête avec les en-têtes
+        ResponseEntity<ClientResponse> response = restTemplate.exchange(
+                url, HttpMethod.GET, entity, ClientResponse.class);
+        
+        return response.getBody();
+    }
 }
